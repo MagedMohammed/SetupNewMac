696902bc9c5ab4a34d7ff2b950c82e5a50f340b5
Maged <maged.mo.it@gmal.com>
2019-08-16 03:07:47 +0200

    new Egit

---
+++ SetupNewMac.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+# ☕☕☕☕
+ ☕ 💪 🕛  🔥
+echo "Welcome to my script"
+echo -e "now download CocoaPods 💻 \a"
+read -p "Please enter your work email: " email
+# insall Homebrew
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+#  install google chrome
+brew cask install google-chrome
+# install firefox 🦊
+brew cask install firefox
+#  install cocoapods
+sudo gem install cocoapods
+#  adobe reader 
+brew cask install adobe-reader
+#  postman
+brew cask install postman
+#  slack
+brew cask install slack 
+# visual studio code
+brew cask install visual-studio-code
+#  atom
+brew cask install atom
+# git
+brew install git
+
+
